55b6bac656af199015a0bc36bbef3fbf35ff7804
Grushaas <grusha_as@outlook.com>
2022-08-14 18:37:08 +0500

    Lesson 5 - Task 1 - Version 1.0

---
+++ Lesson5/Task1/Squaring/Squaring/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+template <class T>
+T squaring(T a)
+{
+	return a * a;
+}
+
+template <class Y>
+void squaringVector(std::vector<Y>& xs)
+{
+	for (int i = 0; i < xs.size(); ++i)
+	{
+		xs[i] *= xs[i];
+	}
+}
+
+int main()
+{
+
+
+	int num = 10;
+	std::vector<int> nums = { -1, 4, 8 };
+	auto print = [](int n)
+	{
+		std::cout << n << " ";
+	};
+
+	std::cout << "[IN]: " << num << std::endl;
+	std::cout << "[OUT]: " << squaring(num) << std::endl;
+	std::cout << "[IN]: ";
+	std::for_each(nums.cbegin(), nums.cend(), print);
+
+	std::cout << std::endl;
+
+	std::cout << "[OUT]: ";
+	squaringVector(nums);
+	std::for_each(nums.cbegin(), nums.cend(), print);
+	
+}
