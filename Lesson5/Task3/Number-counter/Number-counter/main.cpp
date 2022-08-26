48c9ec4a1418ab41dc7a78c4fc6116d49f98685f
Grushaas <grusha_as@outlook.com>
2022-08-26 16:52:46 +0500

    Lesson 5 - Task 3 - Version 1.1

--- Lesson5/Task3/Number-counter/Number-counter/main.cpp
+++ Lesson5/Task3/Number-counter/Number-counter/main.cpp
@@ -7,7 +7,6 @@ class Functor
 private:
 	int count = 0;
 	int sum = 0;
-	std::vector<int> numbers = { 4, 1, 3, 6, 25, 54 };
 
 public:
 	int Get_sum() const
@@ -22,22 +21,20 @@ public:
 
 	void operator()()
 	{
-		auto getsum = [this](int n)
+		int n;
+		if (!(n % 3))
 		{
-			if (!(n % 3))
-			{
-				sum += n;
-				++count;
-			}
-		};
-		std::for_each(numbers.begin(), numbers.end(), getsum);
+			sum += n;
+			++count;
+		}
 	}
 };
 
 int main()
 {
+	std::vector<int> numbers = { 4, 1, 3, 6, 25, 54 };
 	Functor func;
-	func();
+	std::for_each(numbers.begin(), numbers.end(), func);
 	std::cout << func.Get_sum() << std::endl;
 	std::cout << func.Get_count() << std::endl;
 }
