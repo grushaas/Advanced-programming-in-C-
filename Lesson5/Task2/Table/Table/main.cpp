0fd10f2a72f761113eda40875af2d6f2d7df6c3f
Grushaas <grusha_as@outlook.com>
2022-08-30 19:34:15 +0500

    Lesson 5 - Task 2 - Version 1.3

--- Lesson5/Task2/Table/Table/main.cpp
+++ Lesson5/Task2/Table/Table/main.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
+#include <string>
 #include <vector>
 
 template<class T>
 class Table
 {
 private:
-	T const rows = 0;
-	T const cols = 0;
-	T size = 0;
+	int const rows = 0;
+	int const cols = 0;
+	int size = 0;
 	T** arr = nullptr;
 
 public:
-	Table(T r, T c) : rows(r), cols(c)
+	Table(int r, int c) : rows(r), cols(c)
 	{
 		if (rows <= 0 || cols <= 0) throw std::runtime_error("The size of the row or column must be greater than or equal to zero");
-		arr = new int* [rows];
+		arr = new T* [rows];
 		for (int i = 0; i < rows; ++i)
 		{
-			arr[i] = new int[cols];
+			arr[i] = new T[cols];
 		}
 		size = rows * cols;
 	}
@@ -33,17 +34,17 @@ public:
 		
 	}
 
-	T Size() const
+	int Size() const
 	{
 		return size;
 	}
 
-	T* operator[](const T i) const
+	T* operator[](const int i) const
 	{
 		return arr[i];
 	}
 
-	T* operator[](T i)
+	T* operator[](int i)
 	{
 		return arr[i];
 	}
@@ -51,8 +52,8 @@ public:
 
 int main()
 {
-	const Table<int> table = Table<int>(2, 3);
-	table[0][0] = 4;
+	const Table<bool> table = Table<bool>(3, 5);
+	table[0][0] = false;
 	std::cout << table[0][0] << std::endl;
 
 	table.Size();
