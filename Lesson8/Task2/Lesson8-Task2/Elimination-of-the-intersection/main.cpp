5418431832754b4fe33fe098edbc63c962e645d6
Grushaas <grusha_as@outlook.com>
2022-09-02 13:32:27 +0500

    Lesson 8 - Task 2 - Version 1.1

--- Lesson8/Task2/Lesson8-Task2/Elimination-of-the-intersection/main.cpp
+++ Lesson8/Task2/Lesson8-Task2/Elimination-of-the-intersection/main.cpp
@@ -6,7 +6,7 @@ class node
 {
 public:
     int m_value;
-    std::shared_ptr<node> parent;
+    std::weak_ptr<node> parent;
     node(int value) : m_value{ value } {};
     ~node() { std::cout << "destructor called\n"; }
 };
@@ -16,6 +16,8 @@ int main()
     {
         auto node1 = std::make_shared<node>(1);
         auto node2 = std::make_shared<node>(2);
+        node1->parent = node2;
+        node2->parent = node1;
     }
 
     return 0;
